e7569d47b6efb5656286726d8762b5429ec468e6
kevin <bastokevindaniel@gmail.com>
2020-07-15 17:07:51 -0500

    cambios menores de artículos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
   ],
   imports: [
     BrowserModule
